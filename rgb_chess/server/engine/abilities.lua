4ba4033f63f3437c83d5823dc31e55003b96d9af
Oli <OliverWDGarrett@gmail.com>
2023-04-11 12:39:39 +1200

    Started on ability refactor

--- rgb_chess/server/engine/abilities.lua
+++ rgb_chess/server/engine/abilities.lua
@@ -1,139 +1,103 @@
 
-
 local Board = require("server.board")
 
 
+local abilityGroup = umg.group("abilities")
 
-umg.on("rgbAttack", function(atcker_ent, targ_ent, dmg)
-    if targ_ent.onDamage then
-        targ_ent:onDamage(atcker_ent, dmg)
+
+
+--[[
+    Okay... this is a *bit* hacky,
+    but basically all abilities are tied directly to callbacks.
+
+    when the callback is emitted, then the ability is called.
+]]
+local validAbilities = {
+    "onDeath",-- (ent)
+    "onBuff",-- (ent, buffType, amount, buffer_ent, depth )
+    "onDebuff",-- (ent, buffType, amount, buffer_ent, depth )
+    
+    "onBuy",-- (ent)
+    "onSell",-- (ent)
+
+    "onAllySummoned",
+    "onAllySold",
+    
+    "onDamage",
+    "onHeal",
+    "onAttack",
+    "onStun",
+
+    "onAllyDeath",
+    
+    "onReroll",
+    "onStartTurn",
+    "onEndTurn",
+
+    "onStartBattle",
+    "onEndBattle"
+}
+
+for _, ability in ipairs(validAbilities) do
+    validAbilities[ability] = true
+end
+
+
+
+
+local function ensureAbilityMapping(ent)
+    if ent.abilities.abilityMapping then
+        return
     end
-    if atcker_ent.onAttack then
-        atcker_ent:onAttack(targ_ent, dmg)
-    end
-end)
 
-
-
-
-local startBattleEnts = umg.group("onStartBattle")
-local endBattleEnts = umg.group("onEndBattle")
-
-umg.on("startBattle", function()
-    for _, ent in ipairs(startBattleEnts) do
-        ent:onStartBattle()
-    end
-end)
-
-umg.on("endBattle", function()
-    for _, ent in ipairs(endBattleEnts) do
-        ent:onEndBattle()
-    end
-end)
-
-
-
-
-
-local startTurnEnts = umg.group("onStartTurn")
-local endTurnEnts = umg.group("onEndTurn")
-
-umg.on("startTurn", function()
-    for _, ent in ipairs(startTurnEnts) do
-        ent:onStartTurn()
-    end
-end)
-
-umg.on("endTurn", function()
-    for _, ent in ipairs(endTurnEnts) do
-        ent:onEndTurn()
-    end
-end)
-
-
-
-
-local allySummonEnts = umg.group("onSummon")
-local allySoldEnts = umg.group("onAllySold")
-local allyDeathEnts = umg.group("onAllyDeath")
-
-umg.on("summonUnit", function(summoned_ent)
-    for _, e in ipairs(allySummonEnts) do
-        if e.rgbTeam == summoned_ent.rgbTeam and summoned_ent ~= e then
-            e:onAllySummoned(summoned_ent)
+    ent.abilities.abilityMapping = {}
+    for _, ability in ipairs(ent.abilities) do
+        local typ = ability.type or "nil"
+        if not validAbilities[typ] then
+            error("Invalid ability type for entity: " .. ent:type() .. "  " .. typ)
         end
+
+        ent.abilities.abilityMapping[typ] = ability
     end
-end)
+end
 
-umg.on("sellUnit", function(sold_ent)
-    for _, e in ipairs(allySoldEnts) do
-        if e.rgbTeam == sold_ent.rgbTeam and sold_ent ~= e then
-            e:onAllySold(sold_ent)
-        end
+
+
+
+local abilityGroups = {
+--[[
+    [abilityType] -> { ent1, ent2, ... }
+    -- list of entities that contain `abilityType`
+]]
+}
+
+
+
+local getABGroupTc = base.typecheck.assert("string")
+local function getAbilityGroup(abilityType)
+    getABGroupTc(abilityType)
+    if not abilityGroups[abilityType] then
+        abilityGroups[abilityType] = base.Array()
     end
-    if sold_ent.onSell then
-        sold_ent:onSell()
+    return abilityGroups[abilityType]
+end
+
+
+local function addToAbilityGroups(ent)
+    for _, ability in ipairs(ent.abilities) do
+        local group = getAbilityGroup(ability.type)
+        group:add(ent)
     end
-end)
-
-umg.on("entityDeath", function(dead_ent)
-    for _, e in ipairs(allyDeathEnts) do
-        if e.rgbTeam == dead_ent.rgbTeam and dead_ent ~= e then
-            e:onAllyDeath(dead_ent)
-        end
-    end 
-end)
-
-umg.on("buyUnit", function(ent)
-    if ent.onBuy then
-        ent:onBuy()
-    end
-end)
+end
 
 
 
-umg.on("reroll", function(rgbTeam)
-    local board = Board.getBoard(rgbTeam)
-    for _, ent in board:iterUnits() do
-        if ent.onReroll then
-            ent:onReroll()
-        end
-    end
-end)
-
-
-
-local BUFF_TYPES = constants.BUFF_TYPES
-
-umg.on("buff", function(unit, buffType, amount, buffer_ent, depth)
-    assert(BUFF_TYPES[buffType], "???")
-    if unit.onBuff then
-        unit:onBuff(buffType, amount, buffer_ent, depth)
-    end
-end)
-
-umg.on("debuff", function(unit, buffType, amount, buffer_ent, depth)
-    assert(BUFF_TYPES[buffType], "???")
-
-    if unit.onDebuff then
-        unit:onDebuff(buffType, amount, buffer_ent, depth)
-    end
+abilityGroup:onAdded(function(ent)
+    ensureAbilityMapping(ent)
+    addToAbilityGroups(ent)
 end)
 
 
 
 
-
-local shieldBreakGroup = umg.group("onAllyShieldBreak")
-
-umg.on("breakShield", function(ent, shield)
-    for _, e in ipairs(shieldBreakGroup) do
-        if e.rgbTeam == ent.rgbTeam then
-            e:onAllyShieldBreak(ent, shield)
-        end
-    end
-    if ent.onShieldBreak then
-        ent:onShieldBreak(shield)
-    end
-end)
 
