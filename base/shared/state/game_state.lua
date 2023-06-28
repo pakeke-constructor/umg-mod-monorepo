cff2527a5657d46e01bc6ae2ecb1dd93ac4aa739
Oli <OliverWDGarrett@gmail.com>
2023-06-28 12:42:44 +1200

    support for delta time modification

--- base/shared/state/game_state.lua
+++ base/shared/state/game_state.lua
@@ -14,9 +14,17 @@ When the game is running, it should be in this state.
 local gameState = State("game")
 
 
+local constants = require("shared.constants")
+
+local MAX_DT = constants.MAX_DT
+local MIN_DT = constants.MIN_DT
+local max, min = math.max, math.min
 
 
 gameState:on("@update", function(dt)
+    local modif = umg.ask("getDeltaTimeModifier")
+    dt = min(MAX_DT, max(MIN_DT, dt * modif))
+
     umg.call("gamePreUpdate", dt)
     umg.call("gameUpdate", dt)
     umg.call("gamePostUpdate", dt)
