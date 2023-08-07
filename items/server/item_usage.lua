bbdbaf3a3c868c9618880cc51bbee368c7b5e64e
Oli <OliverWDGarrett@gmail.com>
2023-08-07 22:19:51 +1200

    Revert "Moved item usage over to server"
    This reverts commit 8734e738eb1721d2160f5064f629e0fbb4522645.

--- items/server/item_usage.lua
+++ items/server/item_usage.lua
@@ -26,7 +26,7 @@ local function useItemDeny(item, holder_ent, ...)
     if type(item.useItemDeny) == "function" then
         item:useItemDeny(holder_ent, ...)
     end
-    umg.call("usables:useItemDeny", holder_ent, item, ...)
+    umg.call("items:useItemDeny", holder_ent, item, ...)
 end
 
 
@@ -52,7 +52,7 @@ function itemUsage.useItemDirectly(holder_ent, item, ...)
     if type(item.useItem) == "function" then
         item:useItem(holder_ent or false, ...)
     end
-    umg.call("usables:useItem", holder_ent, item, ...)
+    umg.call("items:useItem", holder_ent, item, ...)
     server.broadcast("useItem", holder_ent, item, ...)
     item.itemLastUseTime = state.getGameTime()
 end
