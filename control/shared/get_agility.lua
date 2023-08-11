6e7312ca9ee244549c4ee6d71d7bc2c21e01f2c0
Oli <OliverWDGarrett@gmail.com>
2023-08-12 00:03:17 +1200

    removed reducers from agility questions

--- control/shared/get_agility.lua
+++ control/shared/get_agility.lua
@@ -8,8 +8,8 @@ local DEFAULT_AGILITY = 100
 local function getAgility(ent)
     local agility = ent.agility or DEFAULT_AGILITY
 
-    local agility_add = umg.ask("control:getAgilityModifier", reducers.ADD, ent) or 0
-    local agility_multiplier = umg.ask("control:getAgilityMultiplier", reducers.MULTIPLY, ent) or 1
+    local agility_add = umg.ask("control:getAgilityModifier", ent) or 0
+    local agility_multiplier = umg.ask("control:getAgilityMultiplier", ent) or 1
 
     return (agility + agility_add) * agility_multiplier
 end
