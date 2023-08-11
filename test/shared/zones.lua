667adb250ad0c50bb97a8ac4f365d107ad3fbc15
Oli <OliverWDGarrett@gmail.com>
2023-08-11 12:11:32 +1200

    hacky code to showcase question buses

--- test/shared/zones.lua
+++ test/shared/zones.lua
@@ -14,22 +14,54 @@ end)
 end
 
 
-umg.on("rendering:getScale", function(ent)
 
+local zones
+
+local yelGroup = umg.group("yellowZone")
+local cyanGroup = umg.group("magentaZone")
+local magGroup = umg.group("cyanZone")
+
+umg.on("@tick", function()
+    yellowZone = yelGroup[1]
+    cyanZone = cyanGroup[1]
+    magZone = magGroup[1]
+    zones = {yellowZone, cyanZone, magZone}
 end)
 
 
-umg.on("rendering:getScale", function(ent)
 
+
+local function inRangeOf(ent, zoneEnt)
+    assert(zoneEnt.size, "wot wot")
+    local d = math.distance(ent.x - zoneEnt.x, ent.y - zoneEnt.y)
+    return d < zoneEnt.size
+end
+
+
+
+if client then
+
+
+--[[
+yellow: scale
+
+aqua: 
+
+
+]]
+
+umg.on("rendering:getScale", function(ent)
+    if inRangeOf(ent, yellowZone) then
+        return 2
+    end
 end)
 
-
-umg.on("rendering:getScale", function(ent)
+umg.on("rendering:getScaleX", function(ent)
 
 end)
-
 
 
 umg.on("rendering:getColor", function(ent)
-
 end)
+
+end
