76b5794b07a09b669492772e2efedd55d9d3545b
Oli <OliverWDGarrett@gmail.com>
2023-08-13 16:06:54 +1200

    created DimensionVector object

---
+++ dimensions/REFACTOR_PLAN.md
@@ -0,0 +1,52 @@
+
+
+# dimensions refactor plan:
+
+
+## refactor position passing:
+
+IDEA: Instead of passing `x,y,z,dimension` into every function manually,
+how about we create a `dimensionVector` object of the following shape:
+```lua
+{
+    x: number,
+    y: number,
+    dimension: string?
+    z: number? 
+}
+```
+that way, we can pass positions/dim values around more easily.
+AND WHATS BETTER:
+Is that if we don't want to allocate an object, we can simply pass in
+the entity instead!!! (Amazing.)
+
+
+
+
+
+
+
+
+
+
+
+# What mods need to change:
+
+- chunking
+partition data structure should contain positions,
+AND dimension values for entities.
+
+- rendering
+we need to create Z-indexers for each dimension.
+camera should contain dimension too. 
+there should be an API for switching camera between dimensions.
+
+- physics
+each dimension needs a physics world
+
+
+- juice mod
+particles, sfx, etc use `dimensionVector`
+
+
+
---
+++ objects/shared/dvector.lua
@@ -0,0 +1,76 @@
+
+--[[
+
+DimensionVector class.
+Used to represent a position in the universe.
+(x,y,z and dimension values)
+
+A DimensionVector is an object of the following shape:
+
+dVector {
+    x: number,
+    y: number,
+    dimension: string?
+    z: number? 
+}
+
+
+`dimension` and `z` are optional values.
+z defaults to 0,
+dimension defaults to the default dimension.
+
+x,y values are compulsory.
+
+
+Note that entities can be used in place of dimensionVectors,
+provided they have x,y components.
+
+This makes dimensionVectors super useful, as we can create "abstract"
+functions that can take either a DimensionVector, or an entity.
+
+]]
+
+
+
+
+--[[
+    DimensionVectors don't have any special methods or anything
+    bound to them.
+    They are just POD.
+]]
+
+
+local ndvTc1 = typecheck.assert("number", "number", "string?", "number?")
+local ndvTc2 = typecheck.assert({
+    x = "number",
+    y = "number",
+    dimension = "string?",
+    z = "number?"
+})
+
+local function newDimensionVector(tabl_or_x, y, dimension, z_or_nil)
+    if type(tabl_or_x) == "number" then
+        ndvTc1(tabl_or_x, y, dimension, z_or_nil)
+        return {
+            x = tabl_or_x,
+            y = y,
+            dimension = dimension,
+            z = z_or_nil
+        }
+    end
+
+    if type(tabl_or_x) == "table" then
+        ndvTc2(tabl_or_x)
+        local e = tabl_or_x
+        return {
+            x = e.x,
+            y = e.y,
+            z = e.z,
+            dimension = e.dimension,
+        }
+    end
+end
+
+
+return newDimensionVector
+
