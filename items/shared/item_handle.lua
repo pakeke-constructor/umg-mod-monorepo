ccf99e4d8a43bc68099dbb5ecdd64b1cbb42903c
Oli <OliverWDGarrett@gmail.com>
2023-08-24 12:32:33 +1200

    work on item handles

--- items/shared/item_handle.lua
+++ items/shared/item_handle.lua
@@ -2,21 +2,33 @@
 --[[
     
 An "ItemHandle" is an object representing an item in an inventory slot.
-Item
 
-It's useful, because if that item is moved out of the inventory,
-or if the item moves slots, the handle becomes invalid.
+It's useful when we want items to have more complex behaviour.
 
-This allows other systems to use item handles for other abstract use
-cases, such as:
+If an item is moved out of an inventory, or if an item moves slots,
+any ItemHandle for that item becomes invalid,
+and other systems that have a reference to that ItemHandle will know.
+
+This allows systems to use items in more complex ways, for example:
 - item holding
-- armour
-- passive item buffs
+- armour (ie. chestplate slot)
+- passive item buffs (ie. terraria passive item slots)
+
 
 Each inventory may have ItemHandles.
 ItemHandles are managed by the inventory internally.
 Nothing needs to be done to manage them externally; they are fully
 handled by the inventory itself.
+
+-----------------------
+Systems may also respond to ItemHandles becoming invalid through callbacks
+
+For example, an ItemHandle with the `removePosition` flag would
+cause position components to be removed from the item.
+
+This frees the burden of state from the systems who own the itemHandles.
+They can just add the `removePosition` flag, and be confident that
+everything will be cleaned up.
 
 ]]
 
@@ -27,6 +39,12 @@ local ItemHandle = objects.Class("items:ItemHandle")
 local initTc = typecheck.assert("number", "number", "entity")
 
 function ItemHandle:init(slotX, slotY, itemEnt)
+    --[[
+        `itemEnt` is the item at (slotX, slotY) in the inventory.
+
+        We don't have a reference to the inventory; rather,
+        the inventory has a reference to us.
+    ]]
     initTc(slotX, slotY, itemEnt)
     self.slotX = slotX
     self.slotY = slotY
@@ -36,7 +54,17 @@ end
 
 
 function ItemHandle:isValid()
-    return self.valid
+    if not self.valid then
+        return false
+    end
+
+    if not umg.exists(self._item) then
+        -- if the item ent no longer exists, invalidate self
+        self:invalidate()
+        return false
+    end
+
+    return true
 end
 
 
@@ -44,7 +72,7 @@ function ItemHandle:get()
     if self:isValid() then
         return self._item
     end
-    error("ItemHandle was not valid! check using ih:isValid() before calling ih:get()")
+    error("ItemHandle was not valid! check using :isValid() before calling :get()")
 end
 
 
